dd50859a1e61c283bd5f271bb6f7fcd7f2a89f0e
ku00 <skentaro36@gmail.com>
2016-09-28 18:33:21 +0900

    added classes Configuration and ContractInformation

--- lib/global_sign.rb
+++ lib/global_sign.rb
@@ -1,5 +1,25 @@
 require "global_sign/version"
 
 module GlobalSign
-  # Your code goes here...
+  class << self
+    attr_accessor :configuration, :contract_information
+
+    def configure
+      @configuration ||= Configuration.new
+      yield @configuration if block_given?
+    end
+
+    def contract
+      @contract_information ||= ContractInformation.new
+      yield @contract_information if block_given?
+    end
+  end
+
+  class Configuration
+    attr_accessor :user_name, :password, :endpoint
+  end
+
+  class ContractInformation
+    attr_accessor :first_name, :last_name, :phone_number, :email
+  end
 end
