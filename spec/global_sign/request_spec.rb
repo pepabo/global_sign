3b21fd90a0fcf635b1041a817404855cba013d46
ku00 <skentaro36@gmail.com>
2016-09-30 10:17:14 +0900

    added base classes test

---
+++ spec/global_sign/request_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe GlobalSign::Request do
+  let(:request) do
+    GlobalSign::UrlVerification::Request.new(
+      order_kind: 'new',
+      csr: 'xxxxx',
+      contract_info: GlobalSign.contract_information,
+    )
+  end
+
+  describe '#to_xml' do
+    let(:action_name) { 'URLVerification' }
+    let(:params) { request.auth_token_params.merge(request.params) }
+
+    before do
+      allow(request).to receive(:action).and_return(action_name)
+      allow(request).to receive(:params).and_return({})
+    end
+
+    it 'calls RequestXmlBuilder.build_xml' do
+      expect(GlobalSign::RequestXmlBuilder).to receive(:build_xml).
+        with(action: action_name, params: params)
+      request.to_xml
+    end
+  end
+
+  describe '#auth_token_params' do
+    let(:user_name) { 'PAR12345_hoge' }
+    let(:password) { 'password' }
+
+    before do
+      GlobalSign.configure do |config|
+        config.user_name = user_name
+        config.password = password
+      end
+    end
+
+    it 'returns hash' do
+      expect(request.auth_token_params).to eq(
+        {
+          OrderRequestHeader: {
+            AuthToken: {
+              UserName: user_name,
+              Password: password,
+            }
+          }
+        }
+      )
+    end
+  end
+end
---
+++ spec/global_sign/request_xml_builder_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe GlobalSign::RequestXmlBuilder do
+  describe '.build_xml' do
+    let(:request) do
+      GlobalSign::UrlVerification::Request.new(
+        order_kind: 'new',
+        csr: 'xxxxx',
+        contract_info: GlobalSign.contract_information,
+      )
+    end
+
+    let(:xml) { File.read('spec/fixtures/global_sign_request.xml') }
+
+    subject { GlobalSign::RequestXmlBuilder.build_xml(action: request.action, params: request.params) }
+
+    before do
+      GlobalSign.contract do |contract_information|
+        contract_information.first_name = 'Pepabo'
+        contract_information.last_name = 'Taro'
+        contract_information.phone_number = '090-1234-5678'
+        contract_information.email = 'test@example.com'
+      end
+    end
+
+    it 'returns xml for GlobalSign API request' do
+      is_expected.to eq(Nokogiri::XML(xml).root.to_xml)
+    end
+  end
+end
