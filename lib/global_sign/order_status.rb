2e13d28a46d5afcf59e764761b35f19bbf07807d
ku00 <skentaro36@gmail.com>
2016-10-20 16:14:46 +0900

    fix order status values

--- lib/global_sign/order_status.rb
+++ lib/global_sign/order_status.rb
@@ -2,12 +2,12 @@ module GlobalSign
   class OrderStatus
     STATUS_MAPPING = {
       '1' => 'initial',
-      '2' => 'phishing_checking',
-      '3' => 'cancelled_not_issued',
-      '4' => 'issue_completed',
-      '5' => 'cancelled_issued',
-      '6' => 'revoking',
-      '7' => 'revoked',
+      '2' => 'waiting_for_phishing_check',
+      '3' => 'cancelled_before_issue',
+      '4' => 'completed_issue',
+      '5' => 'cancelled_after_issue',
+      '6' => 'waiting_for_certificate_revocation',
+      '7' => 'revoked_certificate',
     }.each_value(&:freeze).freeze
   end
 end
