dd44ed9d6cafb7e39a28db8478e25ba37ce4db29
hiromikimura <hiromikimura@pepabo.com>
2016-12-19 11:42:05 +0900

    Revert "add option responses"
    This reverts commit 8871c6efbcbb8d33dc6d23495cae4add3fd3e747.

--- lib/global_sign/order_getter_by_order_id/response.rb
+++ lib/global_sign/order_getter_by_order_id/response.rb
@@ -5,12 +5,6 @@ module GlobalSign
         ORDER_ID            = '//Response/OrderID'
         ORDER_STATUS        = '//Response/OrderDetail/OrderInfo/OrderStatus'
         MODIFICATION_EVENTS = '//Response/OrderDetail/ModificationEvents'
-
-        # options
-        CERTIFICATE_INFO    = '//Response/OrderDetail/CertificateInfo'
-        FULFILLMENT         = '//Response/OrderDetail/Fulfillment'
-        CA_CERTIFICATES     = '//Response/OrderDetail/Fulfillment/CACertificates'
-        SERVER_CERTIFICATE  = '//Response/OrderDetail/Fulfillment/ServerCertificate'
       end
 
       def response_header
@@ -18,31 +12,11 @@ module GlobalSign
       end
 
       def params
-        return @params if @params
-        _params = {
+        @params ||= {
           order_id:            @xml.xpath(XPath::ORDER_ID).text,
           order_status:        @xml.xpath(XPath::ORDER_STATUS).text,
           modification_events: modification_events_list
         }
-
-        # options
-        _params[:certificate_info] = {
-          certificate_status: certificate_info.at('CertificateStatus').text,
-          start_date:         certificate_info.at('StartDate').text,
-          end_date:           certificate_info.at('EndDate').text,
-          common_name:        certificate_info.at('CommonName').text,
-          subject_name:       certificate_info.at('SubjectName').text,
-        } if certificate_info.text.present?
-
-        _params[:fulfillment] = {
-            ca_certificates: ca_certificates_list,
-            server_certificate: {
-              x509_cert:  server_certificate.at('X509Cert').text,
-              pkcs7_cert: server_certificate.at('PKCS7Cert').text,
-            }
-        } if fulfillment.text.present?
-
-        @params = _params
       end
 
       def order_status_text
@@ -58,27 +32,6 @@ module GlobalSign
             timestamp: element.at('ModificationEventTimestamp').text,
           }
         end
-      end
-
-      def certificate_info
-        @xml.xpath(XPath::CERTIFICATE_INFO)
-      end
-
-      def fulfillment
-        @xml.xpath(XPath::FULFILLMENT)
-      end
-
-      def ca_certificates_list
-        @xml.xpath(XPath::CA_CERTIFICATES).children.map do |c|
-          {
-            ca_cert_type: c.at('CACertType').text,
-            ca_cert:      c.at('CACert').text,
-          }
-        end
-      end
-
-      def server_certificate
-        @xml.xpath(XPath::SERVER_CERTIFICATE)
       end
     end
   end
