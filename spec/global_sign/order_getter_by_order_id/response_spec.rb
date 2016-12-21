a245cd10bfc7c54cb33eb782932fef55b55ce10d
hiromikimura <hiromikimura@pepabo.com>
2016-12-21 10:07:48 +0900

    add order_getter_by_order_id response test

--- spec/global_sign/order_getter_by_order_id/response_spec.rb
+++ spec/global_sign/order_getter_by_order_id/response_spec.rb
@@ -9,6 +9,23 @@ describe GlobalSign::OrderGetterByOrderId::Response do
     end
   end
 
+  shared_examples_for 'succeeds' do
+    it do
+      expect(@response.success?).to be_truthy
+      expect(@response.error_code).to be_nil
+      expect(@response.error_field).to be_nil
+      expect(@response.error_message).to be_nil
+    end
+  end
+
+  shared_examples_for 'response includes order_getter_by_order_id params' do
+    it do
+      expect(@response.params[:order_id]).to be_present
+      expect(@response.params[:order_status]).to be_present
+      expect(@response.params[:modification_events]).to be_present
+    end
+  end
+
   context 'when returned success response' do
     let(:cassette_title) { 'success' }
 
@@ -16,21 +33,56 @@ describe GlobalSign::OrderGetterByOrderId::Response do
       GlobalSign::OrderGetterByOrderId::Request.new(order_id: 'xxxx123456789')
     end
 
-    it 'succeeds' do
-      expect(@response.success?).to be_truthy
-      expect(@response.error_code).to be_nil
-      expect(@response.error_field).to be_nil
-      expect(@response.error_message).to be_nil
-    end
+    it_behaves_like 'succeeds'
+    it_behaves_like 'response includes order_getter_by_order_id params'
 
-    it 'response includes order_getter_by_order_id params' do
-      expect(@response.params[:order_id]).to be_present
-      expect(@response.params[:order_status]).to be_present
-      expect(@response.params[:modification_events]).to be_present
+    it 'response with options params' do
+      expect(@response.params[:certificate_info]).to be_nil
+      expect(@response.params[:fulfillment]).to be_nil
     end
 
     it 'returns order_status text' do
       expect(@response.order_status_text).to eq('initial')
+    end
+  end
+
+  context 'when returned success response with certificate_info option' do
+    let(:cassette_title) { 'with_certificate_info' }
+
+    let(:request) do
+      GlobalSign::OrderGetterByOrderId::Request.new(order_id: 'xxxx123456789', options: {certificate_info: true})
+    end
+
+    it_behaves_like 'succeeds'
+    it_behaves_like 'response includes order_getter_by_order_id params'
+
+    it 'response includes order_getter_by_order_id params' do
+      expect(@response.params[:certificate_info]).to be_present
+      expect(@response.params[:fulfillment]).to be_nil
+    end
+
+    it 'returns order_status text' do
+      expect(@response.order_status_text).to eq('completed_issue')
+    end
+  end
+
+  context 'when returned success response with fulfillment option' do
+    let(:cassette_title) { 'with_fulfillment' }
+
+    let(:request) do
+      GlobalSign::OrderGetterByOrderId::Request.new(order_id: 'xxxx123456789', options: {fulfillment: true})
+    end
+
+    it_behaves_like 'succeeds'
+    it_behaves_like 'response includes order_getter_by_order_id params'
+
+    it 'response includes order_getter_by_order_id params' do
+      expect(@response.params[:certificate_info]).to be_nil
+      expect(@response.params[:fulfillment]).to be_present
+    end
+
+    it 'returns order_status text' do
+      expect(@response.order_status_text).to eq('completed_issue')
     end
   end
 
