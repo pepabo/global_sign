9c2fbdcd766687fca84a6839921d67ac8982f64a
hiromikimura <hiromikimura@pepabo.com>
2016-12-22 09:11:07 +0900

    fix test order_getter_by_order_id request

--- spec/global_sign/order_getter_by_order_id/request_spec.rb
+++ spec/global_sign/order_getter_by_order_id/request_spec.rb
@@ -2,59 +2,61 @@ require 'spec_helper'
 
 describe GlobalSign::OrderGetterByOrderId::Request do
 
-  subject { GlobalSign::OrderGetterByOrderId::Request.new(order_id: 'xxxx123456789', options: options).params }
+  describe '#params' do
+    subject { GlobalSign::OrderGetterByOrderId::Request.new(order_id: 'xxxx123456789', options: options).params }
 
-  context 'no options' do
-    let(:options) { {} }
-    it do
-      expect(subject).to eq(
-        {
-          OrderID: 'xxxx123456789'
-        }
-      )
-    end
-  end
-
-  context 'add certificate_info option' do
-    let(:options) { { certificate_info: true } }
-    it do
-      expect(subject).to eq(
-        {
-          OrderID: 'xxxx123456789',
-          OrderQueryOption: {
-            ReturnCertificateInfo: true
+    context 'when no options' do
+      let(:options) { {} }
+      it do
+        expect(subject).to eq(
+          {
+            OrderID: 'xxxx123456789'
           }
-        }
-      )
+        )
+      end
     end
-  end
 
-  context 'add fulfillment option' do
-    let(:options) { { fulfillment: true } }
-    it do
-      expect(subject).to eq(
-        {
-          OrderID: 'xxxx123456789',
-          OrderQueryOption: {
-            ReturnFulfillment: true
+    context 'when adds certificate_info option' do
+      let(:options) { { certificate_info: true } }
+      it do
+        expect(subject).to eq(
+          {
+            OrderID: 'xxxx123456789',
+            OrderQueryOption: {
+              ReturnCertificateInfo: true
+            }
           }
-        }
-      )
+        )
+      end
     end
-  end
 
-  context 'add multiple options' do
-    let(:options) { { certificate_info: true, fulfillment: true } }
-    it do
-      expect(subject).to eq(
-        {
-          OrderID: 'xxxx123456789',
-          OrderQueryOption: {
-            ReturnCertificateInfo: true,
-            ReturnFulfillment: true
+    context 'when adds fulfillment option' do
+      let(:options) { { fulfillment: true } }
+      it do
+        expect(subject).to eq(
+          {
+            OrderID: 'xxxx123456789',
+            OrderQueryOption: {
+              ReturnFulfillment: true
+            }
           }
-        }
-      )
+        )
+      end
+    end
+
+    context 'when adds multiple options' do
+      let(:options) { { certificate_info: true, fulfillment: true } }
+      it do
+        expect(subject).to eq(
+          {
+            OrderID: 'xxxx123456789',
+            OrderQueryOption: {
+              ReturnCertificateInfo: true,
+              ReturnFulfillment: true
+            }
+          }
+        )
+      end
     end
   end
 end
